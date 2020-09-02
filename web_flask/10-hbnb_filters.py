a0f2b4f57f57b7d97dbc72ef902b6c196bd83cd4
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-09-02 22:43:28 +0000

    11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """[summary]
+    """
+    sta = storage.all(State).values()
+    ame = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=sta, amenities=ame)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """reload close files storage"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
